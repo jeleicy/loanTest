975f448dae15ff6f6d1c9582d69255239010d195
Jeleicy Figueroa <jeleicyf@ornsoft.com>
2021-11-02 12:46:55 -0400

    new files loan test

---
+++ assets/dist/js/functions.js
@@ -0,0 +1,119 @@
+// This function calculate the loan repayment
+
+function calculate() {
+    event.preventDefault();
+    let datestart = document.getElementById("datestart").value;
+    let loan_amount = document.getElementById("loan_amount").value;
+    let intallment_interval = document.getElementById("intallment_interval").value;
+    let intallment_amount = document.getElementById("intallment_amount").value;
+    let interest = document.getElementById("interest").value;
+
+    let loan_interval = 0;
+
+    loan_amount = formatCurrency(loan_amount);
+    loan_amount = parseFloat(loan_amount);
+
+    intallment_amount = formatCurrency(intallment_amount);
+    intallment_amount = parseFloat(intallment_amount);
+    
+    let loan_interes = ((loan_amount*interest)/100)+loan_amount;
+    loan_interval = loan_interes/intallment_amount;
+
+    calculatePayments(loan_interes, loan_interval, intallment_interval, datestart);
+    
+}
+
+var currentYear = new Date().getFullYear();
+var holidays = ["01/01/"+currentYear,"01/18/"+currentYear,"02/15/"+currentYear,"05/31/"+currentYear,"06/19/"+currentYear,"07/04/"+currentYear,"09/06/"+currentYear,"10/11/"+currentYear,"11/11/"+currentYear,"11/25/"+currentYear,"12/25/"+currentYear];
+
+function calculatePayments(loan_interes, loan_interval, intallment_interval, last_day) {
+    event.preventDefault()
+    let date_payment = '';
+    let amount_payment = '';
+    let payoff_payment = loan_interes;
+    let html = '';
+
+    let next_day = new Date(last_day);
+    $('#table_loan tbody').remove();
+    $('#table_loan').append('<tbody>');
+
+    var i=1;
+    let newPayment = new Intl.NumberFormat("en-US", {minimumIntegerDigits:2, style: "currency", currency: "USD"}).format(payoff_payment);
+    if (isNaN(loan_interval) || last_day=='' || intallment_interval==0) {
+        html = `<tr><td colspan="4"><div class="alert alert-danger" role="alert">The Start Date or Loan Amount or Installment Interval or Installment Amount or Simple Interest Rate is not correct!</div></td></tr>`;
+        $('#table_loan').find('tbody').append(html);
+    } else {
+        while (payoff_payment>0) {
+            next_day = next_day.addDays(0);
+
+            payoff_payment = payoff_payment-loan_interval;
+            newPayment = new Intl.NumberFormat("en-US", {minimumIntegerDigits:2, style: "currency", currency: "USD"}).format(payoff_payment);
+
+            html = `<tr><td>${i}</td><td>${next_day.toLocaleDateString("en-US")}</td><td>${new Intl.NumberFormat("en-US", {minimumIntegerDigits:2, style: "currency", currency: "USD"}).format(loan_interval)}</td><td>${newPayment}</td></tr>`;
+            i++;
+            $('#table_loan').find('tbody').append(html);
+
+            if (intallment_interval==1) next_day = next_day.addDays(1);
+            if (intallment_interval==2) next_day = next_day.addDays(7);
+            if (intallment_interval==3) next_day = next_day.addMonth(1);
+        }
+    }
+}
+
+Date.prototype.addDays = function (days) {
+    const date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    while (isBusinessDay(date)===0 || isBusinessDay(date)===6) {
+        date.setDate(date.getDate() + 1);
+    }
+    return date;
+};
+
+Date.prototype.addMonth = function (months) {
+    const date = new Date(this.valueOf());
+    date.setMonth(date.getMonth() + months);
+    while (isBusinessDay(date)===0 || isBusinessDay(date)===6) {
+        date.setDate(date.getDate() + 1);
+    }
+    return date;
+};
+
+function formatCurrency(x) {
+  return x.replace(",","");
+}
+
+// this function id to determine if the payment day is business day
+function isBusinessDay(date_pay) {
+    let hollidayWeek = 0;
+    
+    for (i=0;i<holidays.length;i++) {
+        hollidayWeek = new Date(holidays[i]);
+        if (hollidayWeek.toLocaleDateString("en-US") === date_pay.toLocaleDateString("en-US")) {
+            return 0;
+        }
+    }
+    let dayOfWeek = date_pay.getDay();
+    return dayOfWeek;
+}
+
+    var holidays_datepicker = [currentYear+"-01-01",currentYear+"-01-18",currentYear+"-02-15",currentYear+"-05-31",currentYear+"-06-19",currentYear+"-07-04",currentYear+"-09-06",currentYear+"-10-11",currentYear+"-11-11",currentYear+"-11-25",currentYear+"-12-25"];
+    $( function() {
+        $( "#datestart" ).datepicker({ 
+            beforeShowDay: function(date){
+                    var datestring = jQuery.datepicker.formatDate('yy-mm-dd', date);
+                    return [ holidays_datepicker.indexOf(datestring) == -1 ];
+                },
+        });
+    } );
+
+    $(document).ready(function(){
+        $(".custom1").inputmask({ 'alias' : 'decimal', 'mask' : "[.**]", rightAlign: true});
+    });
+
+    $(".custom2").inputmask('Regex', {regex: "^[0-9]{1,6}(\\.\\d{1,2})?$", rightAlign: true});
+
+    $(".custom3").inputmask({'alias': 'numeric', allowMinus: false, digits: 2, max: 999.99});
+
+    $(function() {
+        $('.custom4').maskMoney();
+    })
---
+++ index.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Uplyft Capital - Evaluation Project - Jeleicy Figueroa</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/dropdowns/">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
+    
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
+    <link href="assets/dist/css/all.css" rel="stylesheet">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.2.6/jquery.inputmask.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-maskmoney/3.0.2/jquery.maskMoney.min.js"></script>
+
+    <script src="assets/dist/js/functions.js"></script>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .content {
+          margin: 30px;
+          /*border: 1px solid red;*/
+      }
+    </style>
+
+    <!-- Custom styles for this template -->
+    
+  </head>
+
+<body bgcolor="white">
+
+<div class="content">
+
+<div class="row">
+    <form method="post" action="#">
+        <h1>Uplyft Capital - Evaluation Project - Jeleicy Figueroa</h1>
+        <div class="row">
+            <div class="col-md-5">
+                <label for="exampleInputEmail1" class="form-label">Start Date</label>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1"><i class="fas fa-calendar-week"></i></span>
+                    <input required type="text" id="datestart" class="form-control" placeholder="Start Date" aria-label="Start Date" aria-describedby="basic-addon1">
+                </div>
+
+                <label for="exampleInputEmail1" class="form-label">Loan Amount</label>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">$</span>
+                    <input required type="text" id="loan_amount" class="custom4 form-control" value="" placeholder="0,00" aria-label="Loan Amount" aria-describedby="basic-addon1" />
+                </div>
+
+                <label for="exampleInputEmail1" class="form-label">Installment Interval</label>
+                <div class="input-group mb-3">
+                <select class="form-select" id="intallment_interval" aria-label="Default select example">
+                    <option value="0" selected>Select...</option>
+                    <option value="1">Daily</option>
+                    <option value="2">Weekly</option>
+                    <option value="3">Monthly</option>
+                    </select>            
+                </div>
+
+                <label for="exampleInputEmail1" class="form-label">Installment Amount</label>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">$</span>
+                    <input required type="text" id="intallment_amount" class="custom4 form-control" value="" placeholder="0,00" aria-label="Loan Amount" aria-describedby="basic-addon1" />
+                </div>
+
+                <label for="exampleInputEmail1" class="form-label">Simple Interest Rate</label>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">%</span>
+                    <input required type="number" id="interest" maxlength="3" class="form-control" value="" placeholder="0" aria-label="Loan Amount" aria-describedby="basic-addon1" />
+                </div>
+                
+                <label for="exampleInputEmail1" class="form-label">
+                    Note: Payments are not collected on weekends or public holidays.
+                </label>
+
+                <div class="col-12">
+                    <button onclick="calculate()" class="btn btn-primary" type="submit">Calculate Loan</button>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <table class="table table-striped table-hover" id="table_loan">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Date</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Payoff</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+        </table>
+    </div>
+</div>
+
+</body>
+</html>
