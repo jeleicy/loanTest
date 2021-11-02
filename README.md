bbb2494a83efe2189b9744e185e070943e172cb4
Jeleicy Figueroa <jeleicyf@ornsoft.com>
2021-11-02 12:53:03 -0400

    test readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# loanTest
+
+# Jeleicy Figueroa Test
+
+#############################
+LoanTest
+#############################
+
+This is a Loan Repayment Calculator, developed on Javascript, HTML, Bootstrap
+
+The Inputs variables:
+- Start Date
+- Loan Amount ($)
+- Installment Interval (Daily, Weekly, Monthly)
+- Installment Amount ($)
+- Simple Interest Rate (%)
+- Note: Payments are not collected on weekends or public holidays.
+
+The Output:
+- HTML Table with scheduled payments
+- Estimated payoff date
--- index.php
+++ index.php
@@ -24,24 +24,7 @@
     <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-
-      .content {
-          margin: 30px;
-          /*border: 1px solid red;*/
-      }
     </style>
 
     <!-- Custom styles for this template -->
